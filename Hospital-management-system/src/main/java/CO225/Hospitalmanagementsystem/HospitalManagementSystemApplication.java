5c6add49e52a028074aeab5e1f441345270786fd
= <e19236@eng.pdn.ac.lk>
2023-06-04 17:24:46 +0530

    first rest api & database connection

--- Hospital-management-system/src/main/java/CO225/Hospitalmanagementsystem/HospitalManagementSystemApplication.java
+++ Hospital-management-system/src/main/java/CO225/Hospitalmanagementsystem/HospitalManagementSystemApplication.java
@@ -2,8 +2,10 @@ package CO225.Hospitalmanagementsystem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "controller")
 public class HospitalManagementSystemApplication {
 
 	public static void main(String[] args) {
---
+++ Hospital-management-system/src/main/java/controller/StartController.java
@@ -0,0 +1,13 @@
+package controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class StartController {
+
+    @GetMapping("/")
+    public String Hello(){
+        return "Under API development process";
+    }
+}
