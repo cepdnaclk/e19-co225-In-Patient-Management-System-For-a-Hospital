27140ac331ad2e74480d55c8411ff2f6ce2909f6
lahirumenik <115687865+lahirumenik@users.noreply.github.com>
2023-06-04 14:55:03 +0530

    Add files via upload
    spring initializer

---
+++ Hospital-management-system/src/test/java/CO225/Hospitalmanagementsystem/HospitalManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package CO225.Hospitalmanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospitalManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
