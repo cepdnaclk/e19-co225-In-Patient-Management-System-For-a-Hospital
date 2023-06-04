c24d663265894989795b4baa38aa0225ef99a00b
= <e19236@eng.pdn.ac.lk>
2023-06-04 23:06:25 +0530

    db

---
+++ Hospital-management-system/src/main/java/entity/Doctor.java
@@ -0,0 +1,16 @@
+package entity;
+
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Doctor {
+
+}
--- Hospital-management-system/src/main/java/entity/User.java
+++ Hospital-management-system/src/main/java/entity/User.java
@@ -1,4 +1,27 @@
 package entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long user_id;
+    private String password;
+    private String birth_date;
+    private String nic;
+    private String name;
+    private String medical_history;
 }
