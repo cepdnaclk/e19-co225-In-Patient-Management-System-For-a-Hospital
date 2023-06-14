7cd24dd7f5b8cff5bb0af84276c2b451fc2002af
Pumudu Nadun Rajakaruna <115540884+PumuduRajakaruna@users.noreply.github.com>
2023-06-14 15:14:00 +0530

    Update NurseUser.java

--- Hospital-management-system/src/main/java/entity/NurseUser.java
+++ Hospital-management-system/src/main/java/entity/NurseUser.java
@@ -5,7 +5,7 @@ import jakarta.persistence.Embeddable;
 import java.io.Serializable;
 
 @Embeddable
-public class nurseUser implements Serializable {
+public class NurseUser implements Serializable {
     @Column(name = "nurse_records_id")
     private long nurseId;
     @Column(name = "user_records_id")
