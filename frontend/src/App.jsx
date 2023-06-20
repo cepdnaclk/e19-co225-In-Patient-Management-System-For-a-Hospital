b51018d95ec6fbbf230d203afea89c3feb60e7d1
Lakshitha Kaveen <e19095@eng.pdn.ac.lk>
2023-06-21 02:14:43 +0530

    Dashboard is set

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
+import Patient from "./components/Patient"
 
 function App() {
     return (
-        <div className="bg-blue-500 text-white p-4">
-            <h1 className="text-3xl font-bold">Hello </h1>
-            <p className="mt-2">Website under construction</p>
+        <div>
+           <Patient/>
         </div>
     );
 }
---
+++ frontend/src/components/Patient.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Dashboard from "./Dashboard"
+
+function App() {
+    return (
+        <div>
+            <Dashboard/>
+        </div>
+
+    );
+}
+
+export default App;
