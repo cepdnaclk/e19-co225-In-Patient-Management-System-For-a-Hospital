3d6515deb7edc3cc17397ea0f58de7101f2668d0
Lakshitha Kaveen <e19095@eng.pdn.ac.lk>
2023-06-21 01:52:46 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,8 +3,8 @@ import React from 'react';
 function App() {
     return (
         <div className="bg-blue-500 text-white p-4">
-            <h1 className="text-3xl font-bold">Hello Tailwind CSS!</h1>
-            <p className="mt-2">Tailwind CSS is awesome.</p>
+            <h1 className="text-3xl font-bold">Hello </h1>
+            <p className="mt-2">Website under construction</p>
         </div>
     );
 }
