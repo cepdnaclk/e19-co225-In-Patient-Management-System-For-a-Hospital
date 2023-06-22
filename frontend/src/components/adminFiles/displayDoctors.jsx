7ad7d31aa47213b184668c40e1bd027f99d1e5b0
Lakshitha Edirisinghe <e19095@eng.pdn.ac.lk>
2023-06-22 23:15:21 +0530

    Doctor navigation section in ADMIN

---
+++ frontend/src/components/adminFiles/displayDoctors.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function DisplayDoctors() {
+    return (
+        <div>
+            <h1>Doctor content under construction...</h1>
+            <h1>Doctor content under construction...</h1>
+            <h1>Doctor content under construction...</h1>
+            <h1>Doctor content under construction...</h1>
+        </div>
+    );
+}
+
+export default DisplayDoctors;
