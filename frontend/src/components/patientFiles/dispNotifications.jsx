8a69851ea9325adb2d46f79a04ba57725421c99d
Lakshitha Edirisinghe <e19095@eng.pdn.ac.lk>
2023-06-22 23:18:07 +0530

    Notification in PATIENT

---
+++ frontend/src/components/patientFiles/dispNotifications.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function DisplayNotifications() {
+    return (
+        <div>
+            <h1>There are no new notifications yet</h1>
+        </div>
+    );
+}
+
+export default DisplayNotifications;
