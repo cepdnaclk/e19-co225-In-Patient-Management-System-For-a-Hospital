e3df5c83d878d6fb935ba3a4cc7b61a209d038f5
Lakshitha Edirisinghe <e19095@eng.pdn.ac.lk>
2023-06-22 23:15:54 +0530

    Nurse navigation section in ADMIN

---
+++ frontend/src/components/adminFiles/displayNurses.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function DisplayNurses() {
+    return (
+        <div>
+            <h1>Nurse content is on the way</h1>
+        </div>
+    );
+}
+
+export default DisplayNurses;
