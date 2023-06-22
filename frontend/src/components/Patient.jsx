28db7a7a23625ed4e3a823494cbeb6bb42a08ccc
Lakshitha Edirisinghe <e19095@eng.pdn.ac.lk>
2023-06-22 16:47:41 +0530

    Patient Created

--- frontend/src/components/Patient.jsx
+++ frontend/src/components/Patient.jsx
@@ -1,20 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import Navbar from './DashboardItems/Navibar';
 import Sidebar from './DashboardItems/Sidebar';
 
 function Patient() {
+    const [navigationItems, setNavigationItems] = useState([
+        [<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M11.35 3.836c-.065.21-.1.433-.1.664 0 .414.336.75.75.75h4.5a.75.75 0 00.75-.75 2.25 2.25 0 00-.1-.664m-5.8 0A2.251 2.251 0 0113.5 2.25H15c1.012 0 1.867.668 2.15 1.586m-5.8 0c-.376.023-.75.05-1.124.08C9.095 4.01 8.25 4.973 8.25 6.108V8.25m8.9-4.414c.376.023.75.05 1.124.08 1.131.094 1.976 1.057 1.976 2.192V16.5A2.25 2.25 0 0118 18.75h-2.25m-7.5-10.5H4.875c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V18.75m-7.5-10.5h6.375c.621 0 1.125.504 1.125 1.125v9.375m-8.25-3l1.5 1.5 3-3.75" />
+         </svg>
+        ,
+          "Dashboard"
+        ],   
+        [ <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 8.25h19.5M2.25 9h19.5m-16.5 5.25h6m-6 2.25h3m-3.75 3h15a2.25 2.25 0 002.25-2.25V6.75A2.25 2.25 0 0019.5 4.5h-15a2.25 2.25 0 00-2.25 2.25v10.5A2.25 2.25 0 004.5 19.5z" />
+          </svg>
+        ,
+           "Payments"
+        ]
+      ]);
+
     return (
         <React.Fragment>
-            <Navbar/>
-            <Sidebar/>
+            <Navbar />
+            <Sidebar navigationItems={navigationItems}/>
             <div className="p-4 sm:ml-64">
-            <div className="p-4 border-2 border-gray-200 border rounded-lg dark:border-gray-700 mt-14">
-
-            </div>
-            </div>
-            
+                <div className="p-4 border-2 border-gray-200 border rounded-lg dark:border-gray-700 mt-14">
+                    <h1>Content Here</h1>
+                </div>
+            </div>    
         </React.Fragment>
-
     );
 }
 
