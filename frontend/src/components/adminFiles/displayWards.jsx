9a8c546d9e6c21cbe1155881cd4f9d57ad5d0e75
Lakshitha Edirisinghe <e19095@eng.pdn.ac.lk>
2023-06-22 23:16:22 +0530

    Wards navigation section in ADMIN

---
+++ frontend/src/components/adminFiles/displayWards.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function DisplayWards() {
+    return (
+        <div>
+            <h1>Ward is getting ready</h1>
+        </div>
+    );
+}
+
+export default DisplayWards;
