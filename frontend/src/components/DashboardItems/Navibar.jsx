bc16ecfa3cc60b30338e6a9403cbe6ec9d06fb4c
Lakshitha Edirisinghe <e19095@eng.pdn.ac.lk>
2023-06-22 16:49:12 +0530

    Modified Navbar

--- frontend/src/components/DashboardItems/Navibar.jsx
+++ frontend/src/components/DashboardItems/Navibar.jsx
@@ -83,18 +83,13 @@ function Navbar() {
                 >
                   <div className="px-4 py-3" role="none">
                     <p className="text-sm text-gray-900 dark:text-white" role="none">
-                      Neil Sims
+                      Test Account
                     </p>
                     <p className="text-sm font-medium text-gray-900 truncate dark:text-gray-300" role="none">
-                      neil.sims@flowbite.com
+                      test@gmail.com
                     </p>
                   </div>
                   <ul className="py-1" role="none">
-                    <li>
-                      <a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-300 dark:hover:bg-gray-600 dark:hover:text-white" role="menuitem">
-                        Dashboard
-                      </a>
-                    </li>
                     <li>
                       <a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-300 dark:hover:bg-gray-600 dark:hover:text-white" role="menuitem">
                         Settings
@@ -102,7 +97,7 @@ function Navbar() {
                     </li>
                     <li>
                       <a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-300 dark:hover:bg-gray-600 dark:hover:text-white" role="menuitem">
-                        Earnings
+                        123BumbleBee
                       </a>
                     </li>
                     <li>
