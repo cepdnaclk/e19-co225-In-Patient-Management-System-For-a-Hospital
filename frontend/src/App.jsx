87d2e47f355cec503d198b1f7eceadc7daaae361
Lakshitha Edirisinghe <e19095@eng.pdn.ac.lk>
2023-06-22 23:09:46 +0530

    Main Content

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Patient from "./components/Patient"
+import Admin from './components/Admin';
 
 function App() {
     return (
         <div>
-           <Patient/>
+           {/* <Patient/> */}
+           <Admin/>
         </div>
     );
 }
