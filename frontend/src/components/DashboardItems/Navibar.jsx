cbf4c467336f7935d14d7457db1a3f9f4d2ff194
Lakshitha Edirisinghe <e19095@eng.pdn.ac.lk>
2023-06-22 15:36:52 +0530

    Navbar created

--- frontend/src/components/DashboardItems/Navibar.jsx
+++ frontend/src/components/DashboardItems/Navibar.jsx
@@ -38,7 +38,7 @@ function Navbar() {
               </svg>
             </button>
             <span className="self-center text-xl font-semibold sm:text-2xl whitespace-nowrap dark:text-white">
-              DashBoard
+              Welcome "UserName"
             </span>
           </div>
 
