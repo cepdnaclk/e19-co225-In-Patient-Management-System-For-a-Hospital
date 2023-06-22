ccc8d140ea3bf6ea9959b7349742665e705b0af3
Lakshitha Edirisinghe <e19095@eng.pdn.ac.lk>
2023-06-22 23:50:20 +0530

    Sidebar

--- frontend/src/components/DashboardItems/Sidebar.jsx
+++ frontend/src/components/DashboardItems/Sidebar.jsx
@@ -1,27 +1,30 @@
 import React from 'react';
 
 function Sidebar(props) {
-  const { navigationItems } = props;
+  const { navigationItems, onItemClick } = props;
+
+  const handleClick = (value) => {
+    onItemClick(value);
+  };
 
   return (
-<aside id="logo-sidebar" className="fixed top-0 left-0 z-40 w-64 h-screen pt-20 transition-transform -translate-x-full bg-white border-r border-gray-200 sm:translate-x-0 dark:bg-gray-800 dark:border-gray-700" aria-label="Sidebar">
+    <aside id="logo-sidebar" className="fixed top-0 left-0 z-40 w-64 h-screen pt-20 transition-transform -translate-x-full bg-white border-r border-gray-200 sm:translate-x-0 dark:bg-gray-800 dark:border-gray-700" aria-label="Sidebar">
       <div className="h-full px-3 pb-4 overflow-y-auto bg-white dark:bg-gray-800">
         {/* Side Navigation Items */}
         <ul className="space-y-2 font-medium">
-        {navigationItems.map( e => (
+          {navigationItems.map((e) => (
             <li key={e[0]}>
-              <a href="#" className="flex items-center p-2 text-white rounded-lg dark:text-white hover:bg-blue-500 hover:shadow-lg hover:shadow-blue-500/40 active:opacity-[0.85]">                
-                <span className="ml-3">{e[0]}</span>
-                {e[1]}
+              <a className="flex items-center p-2 text-white rounded-lg dark:text-white hover:bg-blue-500 hover:shadow-lg hover:shadow-blue-500/40 active:opacity-[0.85]" style={{ cursor: 'pointer' }} onClick={() => handleClick(e[0])}>
+                <span className="ml-3">{e[1]}</span>
+                  {e[2]}
               </a>
             </li>
-        ))}
+          ))}
         </ul>
         {/* Side Navigation items end */}
-
       </div>
     </aside>
   );
-};
+}
 
 export default Sidebar;
