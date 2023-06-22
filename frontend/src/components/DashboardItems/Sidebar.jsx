5c5339b414e5c176d841300e3ec2c2d58a3cbb23
Lakshitha Edirisinghe <e19095@eng.pdn.ac.lk>
2023-06-22 16:49:46 +0530

    Modified Sidebar

--- frontend/src/components/DashboardItems/Sidebar.jsx
+++ frontend/src/components/DashboardItems/Sidebar.jsx
@@ -1,30 +1,24 @@
 import React from 'react';
 
-function Sidebar() {
+function Sidebar(props) {
+  const { navigationItems } = props;
+
   return (
 <aside id="logo-sidebar" className="fixed top-0 left-0 z-40 w-64 h-screen pt-20 transition-transform -translate-x-full bg-white border-r border-gray-200 sm:translate-x-0 dark:bg-gray-800 dark:border-gray-700" aria-label="Sidebar">
       <div className="h-full px-3 pb-4 overflow-y-auto bg-white dark:bg-gray-800">
         {/* Side Navigation Items */}
         <ul className="space-y-2 font-medium">
-          {/* Sidebar items change with the user */}
-          <li>
-            <a href="#" className="flex items-center p-2 text-white rounded-lg dark:text-white hover:bg-blue-500 hover:shadow-lg hover:shadow-blue-500/40 active:opacity-[0.85]">
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
-                <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
-              </svg>                   
-              <span className="ml-3">Dashboard</span>
-            </a>
-          </li>
-          <li>
-            <a href="#" className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-blue-500 hover:shadow-lg hover:shadow-blue-500/40 active:opacity-[0.85]">
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
-                <path strokeLinecap="round" strokeLinejoin="round" d="M9 8.25H7.5a2.25 2.25 0 00-2.25 2.25v9a2.25 2.25 0 002.25 2.25h9a2.25 2.25 0 002.25-2.25v-9a2.25 2.25 0 00-2.25-2.25H15M9 12l3 3m0 0l3-3m-3 3V2.25" />
-              </svg>                   
-              <span className="flex-1 ml-3 whitespace-nowrap">Item 1</span>  
-            </a>
-          </li>
+        {navigationItems.map( e => (
+            <li key={e[0]}>
+              <a href="#" className="flex items-center p-2 text-white rounded-lg dark:text-white hover:bg-blue-500 hover:shadow-lg hover:shadow-blue-500/40 active:opacity-[0.85]">                
+                <span className="ml-3">{e[0]}</span>
+                {e[1]}
+              </a>
+            </li>
+        ))}
         </ul>
         {/* Side Navigation items end */}
+
       </div>
     </aside>
   );
