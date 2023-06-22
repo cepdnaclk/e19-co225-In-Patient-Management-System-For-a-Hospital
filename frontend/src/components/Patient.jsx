19ef3a2b20dbc4b0c8212e3d39d34ed71ee3ab9f
Lakshitha Edirisinghe <e19095@eng.pdn.ac.lk>
2023-06-22 15:40:28 +0530

    Patient Created

--- frontend/src/components/Patient.jsx
+++ frontend/src/components/Patient.jsx
@@ -1,13 +1,21 @@
 import React from "react";
-import Dashboard from "./Dashboard"
+import Navbar from './DashboardItems/Navibar';
+import Sidebar from './DashboardItems/Sidebar';
 
-function App() {
+function Patient() {
     return (
-        <div>
-            <Dashboard/>
-        </div>
+        <React.Fragment>
+            <Navbar/>
+            <Sidebar/>
+            <div className="p-4 sm:ml-64">
+            <div className="p-4 border-2 border-gray-200 border rounded-lg dark:border-gray-700 mt-14">
+
+            </div>
+            </div>
+            
+        </React.Fragment>
 
     );
 }
 
-export default App;
+export default Patient;
