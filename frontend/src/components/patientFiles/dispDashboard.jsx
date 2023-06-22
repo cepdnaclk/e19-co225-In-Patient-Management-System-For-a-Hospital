8fbde67e52469eaba96eed85409056aa46b43acb
Lakshitha Edirisinghe <e19095@eng.pdn.ac.lk>
2023-06-22 23:17:31 +0530

    Dashboard in PATIENT

---
+++ frontend/src/components/patientFiles/dispDashboard.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function DisplayDashboard() {
+    return (
+        <div>
+            <h1>Dashboard is getting ready</h1>
+        </div>
+    );
+}
+
+export default DisplayDashboard;
