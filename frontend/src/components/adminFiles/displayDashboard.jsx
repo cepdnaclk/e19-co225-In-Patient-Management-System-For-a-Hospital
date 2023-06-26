36a059ef0738e5f9922aef07af6b0c20d280bc8c
Lakshitha Edirisinghe <e19095@eng.pdn.ac.lk>
2023-06-27 02:54:53 +0530

    Set Admin Dashboard

--- frontend/src/components/adminFiles/displayDashboard.jsx
+++ frontend/src/components/adminFiles/displayDashboard.jsx
@@ -12,7 +12,7 @@ function AdminDashboard(props) {
   return (
     <>
       <Adminsidebar isMobile={isMobile} />
-      <div className={` ${open ? "left-72 w-[calc(100%-288px)]" : "left-20 w-[calc(100%-80px)]"} absolute p-2`} style={{ backgroundColor: "red", }}>
+      <div className={` ${open ? "left-72 w-[calc(100%-288px)]" : "left-20 w-[calc(100%-80px)]"} absolute p-2`}>
 
         <div className="rounded-tl-3xl bg-gradient-to-r from-blue-900 to-gray-800 p-4 shadow text-2xl text-white" style={{ borderTopLeftRadius: "8px", borderTopRightRadius: "8px" }}>
           <h1 className="font-bold pl-2"> Dashboard </h1>
