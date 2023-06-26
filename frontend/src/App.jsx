b3d94bb24bb82675979daf7fe1166bd9ecdc6cc4
Lakshitha Edirisinghe <e19095@eng.pdn.ac.lk>
2023-06-27 00:26:51 +0530

    Make Responsive Web page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,21 +1,39 @@
-import React from 'react';
-import Patient from "./components/Patient"
-import Admin from './components/Admin';
-import AdminAddStaff from './components/adminFiles/AdminAddStaff';
+import React, { useState, useEffect } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
+import Admin from './components/Admin';
+import AdminAddStaff from './components/adminFiles/AdminAddStaff';
+import PatientDashboard from './components/patientFiles/patientDashboard';
+
 function App() {
+    const [isMobile, setIsMobile] = useState(false);
+
+    useEffect(() => {
+        const handleResize = () => {
+            setIsMobile(window.innerWidth > 600);
+        };
+
+        // Add event listener to window resize
+        window.addEventListener('resize', handleResize);
+
+        // Initial check on component mount
+        handleResize();
+
+        // Clean up event listener on component unmount
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
+    }, []);
+
     return (
-        // <>
-        // <BrowserRouter>
-            <Routes>
-                <Route index element = {<Admin/>}/>
-                <Route path="/" element= {  <Admin/>    }  />
-                <Route path="/patient" element= {  <Patient/>  } />   
-                <Route path="/admin/addStaff" element= {  <AdminAddStaff/>    }  />
-            </Routes>
-        // </BrowserRouter>
-        // </>
+        <Routes>
+            <Route index element={<Admin />} />
+            <Route path="/" element={<Admin />} />
+            <Route path="/admin/addStaff" element={<AdminAddStaff />} />
+
+            <Route path="/patient" element={<PatientDashboard isMobile={isMobile} />} />
+            <Route path="/patient/dashboard" element={<PatientDashboard isMobile={isMobile} />} />
+        </Routes>
     );
 }
 
