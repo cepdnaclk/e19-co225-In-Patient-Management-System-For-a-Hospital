7a54e9d42a605ce9ec04efccaa45ea6e8e65d033
lahirumenik <e19236@eng.pdn.ac.lk>
2023-06-30 01:49:32 +0530

    add material UI and start creating data grids

---
+++ frontend/src/components/adminFiles/DoctorsTable.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const DoctorsTable = () => {
+  return (
+    <div className="flex max-w-2xl mx-auto shadow border-b ">
+    
+    </div>
+  )
+}
+
+export default DoctorsTable
