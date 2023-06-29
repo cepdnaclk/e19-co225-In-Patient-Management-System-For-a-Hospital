a44dd69a3584bf931fd5dcb651eb29cc00fc4cca
Chamue19 <115542082+Chamue19@users.noreply.github.com>
2023-06-29 20:06:56 +0530

    trial3

--- frontend/src/components/adminFiles/displayDashboard.jsx
+++ frontend/src/components/adminFiles/displayDashboard.jsx
@@ -8,6 +8,7 @@ function AdminDashboard(props) {
   const totalDoctors = 87;  //Change where necessary
   const totalNurses = 152;
   const totalUsers = 249;
+  const totalwards = 24;
 
   return (
     <>
@@ -22,7 +23,7 @@ function AdminDashboard(props) {
           {/* Doctors, Nurse, User count */}
           <div className="flex flex-wrap">
             <div className="w-full md:w-1/2 xl:w-1/3 p-6">
-              <div className="bg-gradient-to-b from-green-200 to-green-100 border-b-4 border-green-600 rounded-lg shadow-xl p-5">
+              <div className="bg-gradient-to-b from-green-200 to-green-100 border-b-4 border-yellow-600 rounded-lg shadow-xl p-5">
                 <div className="flex flex-row items-center">
 
                   <div className="flex-shrink pr-4">
@@ -41,7 +42,7 @@ function AdminDashboard(props) {
             </div>
 
             <div className="w-full md:w-1/2 xl:w-1/3 p-6">
-              <div className="bg-gradient-to-b from-blue-200 to-blue-100 border-b-4 border-blue-500 rounded-lg shadow-xl p-5">
+              <div className="bg-gradient-to-b from-blue-200 to-blue-100 border-b-4 border-yellow-500 rounded-lg shadow-xl p-5">
                 <div className="flex flex-row items-center">
                   <div className="flex-shrink pr-4">
                     <div className="rounded-full p-5 bg-blue-600">
@@ -57,10 +58,10 @@ function AdminDashboard(props) {
             </div>
 
             <div className="w-full md:w-1/2 xl:w-1/3 p-6">
-              <div className="bg-gradient-to-b from-pink-200 to-pink-100 border-b-4 border-pink-500 rounded-lg shadow-xl p-5">
+              <div className="bg-gradient-to-b from-red-200 to-red-100 border-b-4 border-yellow-500 rounded-lg shadow-xl p-5">
                 <div className="flex flex-row items-center">
                   <div className="flex-shrink pr-4">
-                    <div className="rounded-full p-5 bg-pink-600">
+                    <div className="rounded-full p-5 bg-red-600">
                       {/* image here */}
                     </div>
                   </div>
@@ -72,7 +73,31 @@ function AdminDashboard(props) {
               </div>
             </div>
           </div>
-        </div>
+        
+
+
+
+        
+            <div className="w-full md:w-1/2 xl:w-1/3 p-6">
+              <div className="bg-gradient-to-b from-purple-200 to-yellow-100 border-b-4 border-yellow-500 rounded-lg shadow-xl p-5">
+                <div className="flex flex-row items-center">
+                  <div className="flex-shrink pr-4">
+                    <div className="rounded-full p-5 bg-purple-600">
+                      {/* image here */}
+                    </div>
+                  </div>
+                  <div className="flex-1 text-right md:text-center">
+                    <h2 className="font-bold uppercase text-gray-600">Total Wards</h2>
+                    <p className="font-bold text-3xl">{totalwards}</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+      
+
+
+        
 
         {/* Add content Here */}
 
