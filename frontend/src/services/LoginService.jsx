6555a3beff02f310bec0264e5f3d7036eb544e0f
lahirumenik <e19236@eng.pdn.ac.lk>
2023-07-01 23:49:29 +0530

    add log in and log out to loginservice

--- frontend/src/services/LoginService.jsx
+++ frontend/src/services/LoginService.jsx
@@ -1,12 +1,44 @@
 import axios from "axios";
 
-const LOGIN_API_BASE_URL = "http://localhost:9081/login";
+const API_URL = "http://localhost:9081";
 
 
-class LoginService {
-    login(user){
-        return axios.post(LOGIN_API_BASE_URL, user);
-    }
-}
+// class LoginService {
+//     login(user){
+//         return axios.post(LOGIN_API_BASE_URL, user);
+//     }
+// }
 
-export default new LoginService();
+// export default new LoginService();
+
+
+
+
+const login = (user) => {
+  return axios
+    .post(API_URL + "/login", 
+     user
+)
+    .then((response) => {
+      if (response.data.accessToken) {
+        localStorage.setItem(response.data.id, JSON.stringify(response.data));
+      }
+
+      return response.data;
+    });
+};
+
+const logout = (id) => {
+  localStorage.removeItem(id);
+};
+
+
+
+const LoginService = {
+  signup,
+  login,
+  logout
+  
+};
+
+export default LoginService;
