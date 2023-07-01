2ad430532b2d16ced06f940dabf4a062b960c302
lahirumenik <e19236@eng.pdn.ac.lk>
2023-07-02 00:09:33 +0530

    fetch acces token to front end succesfully

--- frontend/src/components/loginfiles/Login.jsx
+++ frontend/src/components/loginfiles/Login.jsx
@@ -3,11 +3,12 @@ import { useNavigate } from "react-router-dom";
 import { FaHandHoldingMedical } from "react-icons/fa";
 // import loginImage from './loginimage1.png';
 import loginImage from './loginimage2.jpg';
+import LoginService from "../../services/LoginService";
 
 function Login() {
 
     const [user, setUser] = useState({
-        userName: "",
+        username: "",
         password: "",
       });
 
@@ -26,8 +27,16 @@ function Login() {
     };
     const login = (e) =>{
         e.preventDefault();
+        LoginService.login(user)
+        .then((response) => {
+            console.log(response);
+        })
+        .catch((e)=>{
+            console.log(e);
+        }) 
+        }
 
-    }
+    
 
     return (
         <section className=" select-none h-screen flex flex-col md:flex-row justify-center space-y-10 md:space-y-0 md:space-x-16 items-center my-2 mx-5 md:mx-0 md:my-0">
@@ -42,16 +51,16 @@ function Login() {
                     <p className="mx-4 mb-0 text-center font-semibold text-slate-500 text-6xl">MEDNET</p>
                 </div>
                 <input className="text-sm w-full px-4 py-2 border border-solid border-gray-300 rounded" 
-                name='userName' onChange={(e)=>handleChange(e)} type="text" placeholder="Username" />
+                name='username' onChange={(e)=>handleChange(e)} type="text" placeholder="Username" />
                 <input className="text-sm w-full px-4 py-2 border border-solid border-gray-300 rounded mt-4"
                 name='password' onChange={(e)=>handleChange(e)} type="password" placeholder="Password" />
                 <div className="text-center md:text-left">
                     <button className="mt-4 bg-blue-600 hover:bg-blue-700 px-4 py-2
-                     text-white uppercase rounded text-xs tracking-wider" type="submit" onClick={login}>Login</button>
+                     text-white uppercase rounded text-xs tracking-wider" type="submit" onClick={(e)=>login(e)}>Login</button>
                 </div>
             </div>
         </section>
     );
-}
+    }
 
 export default Login;
