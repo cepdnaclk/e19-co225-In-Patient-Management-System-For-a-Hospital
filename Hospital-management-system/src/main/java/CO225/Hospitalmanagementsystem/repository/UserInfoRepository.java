959de9b38d3131e62f359d78a91a95dffc3ba191
lahirumenik <e19236@eng.pdn.ac.lk>
2023-07-02 02:18:27 +0530

    add backend service to get no.of wards, doctors, .....

--- Hospital-management-system/src/main/java/CO225/Hospitalmanagementsystem/repository/UserInfoRepository.java
+++ Hospital-management-system/src/main/java/CO225/Hospitalmanagementsystem/repository/UserInfoRepository.java
@@ -41,4 +41,27 @@ public interface UserInfoRepository extends JpaRepository <UserInfo, Long> {
             nativeQuery = true
     )
     String getRolesByUserName(String username);
+
+    @Query(
+            value = "select id from user_info where user_name = ?1",
+            nativeQuery = true
+    )
+    Long getIdByUserName(String username);
+
+    @Query(
+            value = "select COUNT(user_name) from user_info where roles = ROLE_DOCTOR",
+            nativeQuery = true
+    )
+    int getDoctorCount();
+
+    @Query(
+            value = "select COUNT(user_name) from user_info where roles = ROLE_NURSE",
+            nativeQuery = true
+    )
+    int getNurseCount();
+    @Query(
+            value = "select COUNT(user_name) from user_info where roles = ROLE_CLERKE",
+            nativeQuery = true
+    )
+    int getClerkCount();
 }
