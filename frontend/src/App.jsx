b8e0e335582f636d634bbcca0eecae813f6fbc0f
Rajitha19 <115542350+Rajitha19@users.noreply.github.com>
2023-07-02 12:50:19 +0100

    Add patient details to doctordash

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -14,6 +14,7 @@ import PatientProfile from './components/patientFiles/patientProfile';
 
 import DoctorDashboard from './components/doctorFiles/doctorDashboard';
 import DoctorProfile from './components/doctorFiles/doctorProfile';
+import PatientHealthDetails from './components/doctorFiles/patients';
 
 import NurseDashboard from './components/NurseFiles/NurseDashboard';
 import NurseProfile from './components/NurseFiles/NurseProfile';
@@ -76,6 +77,7 @@ function App() {
             <Route path="/doctor" element={<DoctorDashboard isMobile={isMobile} />} />
             <Route path="/doctor/dashboard" element={<DoctorDashboard isMobile={isMobile} />} />
             <Route path="/doctor/profile" element={<DoctorProfile isMobile={isMobile} />} />
+            <Route path="/doctor/patients" element={<PatientHealthDetails isMobile={isMobile} />} />
 
             <Route path="/nurse" element={<NurseDashboard isMobile={isMobile} />} />
             <Route path="/nurse/dashboard" element={<NurseDashboard isMobile={isMobile} />} />
