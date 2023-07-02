5394c316710c80ca8a8a85c8b2de748683df43b8
lahirumenik <e19236@eng.pdn.ac.lk>
2023-07-02 18:33:07 +0530

    frot end dashboard complete

--- frontend/src/components/adminFiles/AdminDashboard.jsx
+++ frontend/src/components/adminFiles/AdminDashboard.jsx
@@ -77,7 +77,7 @@ const [test, setTest] = useState([
         console.log(dateParts);
         console.log([year, month,day])
         xValues.push(new Date(year, month, day));
-        nData.push({y:parseInt(item.y), x: new Date(Date.UTC(year, month, day))});
+        nData.push({y:parseInt(item.y), x: (new Date(Date.UTC(year, month, day))).getDate()});
         yValues.push(parseInt(item.y));
       });
         console.log(date);
@@ -131,103 +131,103 @@ const [test, setTest] = useState([
 //     fetchData();
 //   }, []);
 
-  const years = [
-    new Date(1990, 0, 1),
-    new Date(1991, 0, 1),
-    new Date(1992, 0, 1),
-    new Date(1993, 0, 1),
-    new Date(1994, 0, 1),
-    new Date(1995, 0, 1),
-    new Date(1996, 0, 1),
-    new Date(1997, 0, 1),
-    new Date(1998, 0, 1),
-    new Date(1999, 0, 1),
-    new Date(2000, 0, 1),
-    new Date(2001, 0, 1),
-    new Date(2002, 0, 1),
-    new Date(2003, 0, 1),
-    new Date(2004, 0, 1),
-    new Date(2005, 0, 1),
-    new Date(2006, 0, 1),
-    new Date(2007, 0, 1),
-    new Date(2008, 0, 1),
-    new Date(2009, 0, 1),
-    new Date(2010, 0, 1),
-    new Date(2011, 0, 1),
-    new Date(2012, 0, 1),
-    new Date(2013, 0, 1),
-    new Date(2014, 0, 1),
-    new Date(2015, 0, 1),
-    new Date(2016, 0, 1),
-    new Date(2017, 0, 1),
-    new Date(2018, 0, 1),
-  ];
+//   const years = [
+//     new Date(1990, 0, 1),
+//     new Date(1991, 0, 1),
+//     new Date(1992, 0, 1),
+//     new Date(1993, 0, 1),
+//     new Date(1994, 0, 1),
+//     new Date(1995, 0, 1),
+//     new Date(1996, 0, 1),
+//     new Date(1997, 0, 1),
+//     new Date(1998, 0, 1),
+//     new Date(1999, 0, 1),
+//     new Date(2000, 0, 1),
+//     new Date(2001, 0, 1),
+//     new Date(2002, 0, 1),
+//     new Date(2003, 0, 1),
+//     new Date(2004, 0, 1),
+//     new Date(2005, 0, 1),
+//     new Date(2006, 0, 1),
+//     new Date(2007, 0, 1),
+//     new Date(2008, 0, 1),
+//     new Date(2009, 0, 1),
+//     new Date(2010, 0, 1),
+//     new Date(2011, 0, 1),
+//     new Date(2012, 0, 1),
+//     new Date(2013, 0, 1),
+//     new Date(2014, 0, 1),
+//     new Date(2015, 0, 1),
+//     new Date(2016, 0, 1),
+//     new Date(2017, 0, 1),
+//     new Date(2018, 0, 1),
+//   ];
 
 
-  const data = [
-    {
-      "id": "japan",
-      "color": "hsl(63, 70%, 50%)",
-      "data": [
-        {
-          "x": "plane",
-          "y": 290
-        },
-        {
-          "x": "helicopter",
-          "y": 41
-        },
-        {
-          "x": "boat",
-          "y": 54
-        },
-        {
-          "x": "train",
-          "y": 63
-        },
-        {
-          "x": "subway",
-          "y": 214
-        },
-        {
-          "x": "bus",
-          "y": 44
-        },
-        {
-          "x": "car",
-          "y": 221
-        },
-        {
-          "x": "moto",
-          "y": 211
-        },
-        {
-          "x": "bicycle",
-          "y": 71
-        },
-        {
-          "x": "horse",
-          "y": 107
-        },
-        {
-          "x": "skateboard",
-          "y": 251
-        },
-        {
-          "x": "others",
-          "y": 187
-        }
-      ]
-    },
+//   const data = [
+//     {
+//       "id": "japan",
+//       "color": "hsl(63, 70%, 50%)",
+//       "data": [
+//         {
+//           "x": "plane",
+//           "y": 290
+//         },
+//         {
+//           "x": "helicopter",
+//           "y": 41
+//         },
+//         {
+//           "x": "boat",
+//           "y": 54
+//         },
+//         {
+//           "x": "train",
+//           "y": 63
+//         },
+//         {
+//           "x": "subway",
+//           "y": 214
+//         },
+//         {
+//           "x": "bus",
+//           "y": 44
+//         },
+//         {
+//           "x": "car",
+//           "y": 221
+//         },
+//         {
+//           "x": "moto",
+//           "y": 211
+//         },
+//         {
+//           "x": "bicycle",
+//           "y": 71
+//         },
+//         {
+//           "x": "horse",
+//           "y": 107
+//         },
+//         {
+//           "x": "skateboard",
+//           "y": 251
+//         },
+//         {
+//           "x": "others",
+//           "y": 187
+//         }
+//       ]
+//     },
     
-  ];
+//   ];
   
-  const FranceGDPperCapita = [
-    28129, 28294.264, 28619.805, 28336.16, 28907.977, 29418.863, 29736.645, 30341.807,
-    31323.078, 32284.611, 33409.68, 33920.098, 34152.773, 34292.03, 35093.824,
-    35495.465, 36166.16, 36845.684, 36761.793, 35534.926, 36086.727, 36691, 36571,
-    36632, 36527, 36827, 37124, 37895, 38515.918,
-  ];
+//   const FranceGDPperCapita = [
+//     28129, 28294.264, 28619.805, 28336.16, 28907.977, 29418.863, 29736.645, 30341.807,
+//     31323.078, 32284.611, 33409.68, 33920.098, 34152.773, 34292.03, 35093.824,
+//     35495.465, 36166.16, 36845.684, 36761.793, 35534.926, 36086.727, 36691, 36571,
+//     36632, 36527, 36827, 37124, 37895, 38515.918,
+//   ];
   
   
   
@@ -325,7 +325,7 @@ const [test, setTest] = useState([
         </Box>
 </Box>
 
-<LineChart
+{/* <LineChart
       xAxis={[
         {
           id: 'Years',
@@ -350,8 +350,8 @@ const [test, setTest] = useState([
       width={800}
       height={400}
       margin={{ left: 70 }}
-    />
-    <Box height={500}>
+    /> */}
+    <Box height={400}>
       <Linearchart data={test}/>
       </Box>
 
--- frontend/src/components/linearchart.jsx
+++ frontend/src/components/linearchart.jsx
@@ -1,73 +1,126 @@
-import React from 'react'
 import { ResponsiveLine } from '@nivo/line'
-
-const linearchart = ({ data /* see data tab */ }) => (
-    <ResponsiveLine
+import { useTheme } from "@mui/material";
+import { tokens } from "./theme";
+// make sure parent container have a defined height when using
+// responsive component, otherwise height will be 0 and
+// no chart will be rendered.
+// website examples showcase many properties,
+// you'll often use just a few of them.
+const linearchart = ({ data , isCustomLineColors = false, isDashboard = true/* see data tab */ }) => {
+    const theme = useTheme();
+    const colors = tokens(theme.palette.mode);
+    const customTheme = {
+        ...theme,
+        line: {
+          ...theme.line,
+          stroke: '#004d40',
+        },
+    }
+  
+    return (
+      <ResponsiveLine
         data={data}
+        theme={{
+          axis: {
+            domain: {
+              line: {
+                stroke: colors.grey[100],
+              },
+            },
+            legend: {
+              text: {
+                fill: colors.grey[100],
+              },
+            },
+            ticks: {
+              line: {
+                stroke: colors.grey[100],
+                strokeWidth: 1,
+              },
+              text: {
+                fill: colors.grey[100],
+              },
+            },
+          },
+          legends: {
+            text: {
+              fill: colors.grey[100],
+            },
+          },
+          tooltip: {
+            container: {
+              color: colors.primary[500],
+            },
+          },
+        }}
+        colors={{ scheme: 'set1' }} // added
         margin={{ top: 50, right: 110, bottom: 50, left: 60 }}
-        xScale={{ 
-            //type: 'point' 
-            type: "time"
-            }}
+        xScale={{ type: "point" }}
         yScale={{
-            type: 'linear',
-            min: 'auto',
-            max: 'auto',
-            stacked: true,
-            reverse: false
+          type: "linear",
+          min: "auto",
+          max: "auto",
+          stacked: true,
+          reverse: false,
         }}
         yFormat=" >-.2f"
+    
         axisTop={null}
         axisRight={null}
         axisBottom={{
-            tickSize: 5,
-            tickPadding: 5,
-            tickRotation: 0,
-            legend: 'transportation',
-            legendOffset: 36,
-            legendPosition: 'middle'
+          orient: "bottom",
+          tickSize: 0,
+          tickPadding: 15,
+          tickRotation: 0,
+          legend: isDashboard ? undefined : "Patients recorded", // added
+          legendOffset: 36,
+          legendPosition: "middle",
         }}
         axisLeft={{
-            tickSize: 5,
-            tickPadding: 5,
-            tickRotation: 0,
-            legend: 'count',
-            legendOffset: -40,
-            legendPosition: 'middle'
+          orient: "left",
+          tickValues: 5, // added
+          tickSize: 3,
+          tickPadding: 5,
+          tickRotation: 0,
+          legend: isDashboard ? undefined : "count", // added
+          legendOffset: -40,
+          legendPosition: "middle",
         }}
-        pointSize={10}
-        pointColor={{ theme: 'background' }}
+        enableGridX={true}
+        enableGridY={true}
+        pointSize={8}
+        pointColor={{ theme: "background" }}
         pointBorderWidth={2}
-        pointBorderColor={{ from: 'serieColor' }}
+        pointBorderColor={{ from: "serieColor" }}
         pointLabelYOffset={-12}
         useMesh={true}
         legends={[
-            {
-                anchor: 'bottom-right',
-                direction: 'column',
-                justify: false,
-                translateX: 100,
-                translateY: 0,
-                itemsSpacing: 0,
-                itemDirection: 'left-to-right',
-                itemWidth: 80,
-                itemHeight: 20,
-                itemOpacity: 0.75,
-                symbolSize: 12,
-                symbolShape: 'circle',
-                symbolBorderColor: 'rgba(0, 0, 0, .5)',
-                effects: [
-                    {
-                        on: 'hover',
-                        style: {
-                            itemBackground: 'rgba(0, 0, 0, .03)',
-                            itemOpacity: 1
-                        }
-                    }
-                ]
-            }
+          {
+            anchor: "bottom-right",
+            direction: "column",
+            justify: false,
+            translateX: 100,
+            translateY: 0,
+            itemsSpacing: 0,
+            itemDirection: "left-to-right",
+            itemWidth: 80,
+            itemHeight: 20,
+            itemOpacity: 0.75,
+            symbolSize: 12,
+            symbolShape: "circle",
+            symbolBorderColor: "rgba(0, 0, 0, .5)",
+            effects: [
+              {
+                on: "hover",
+                style: {
+                  itemBackground: "rgba(0, 0, 0, .03)",
+                  itemOpacity: 1,
+                },
+              },
+            ],
+          },
         ]}
-    />
-)
-
-export default linearchart
+      />
+    );
+    }
+export default linearchart;
