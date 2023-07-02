d98b5d58a8a1a7d4e9a62c5ebd7eb8427ce06cd1
lahirumenik <e19236@eng.pdn.ac.lk>
2023-07-02 05:34:28 +0530

    add admin dashboard count items with using data from backend

--- frontend/src/components/adminFiles/AdminDashboard.jsx
+++ frontend/src/components/adminFiles/AdminDashboard.jsx
@@ -1,17 +1,46 @@
-import React, { useState } from 'react';
+import { MdPeopleAlt } from "react-icons/md"; 
+import { CiReceipt } from "react-icons/ci"; 
+import { TbNurse } from "react-icons/tb"; 
+import React, { useState, useEffect } from 'react';
 import Navbar from "./Navbar";
 import Doctors from "./DoctorsTable";
 import AdminSideBar from "./AdminsideBar";
 import { Box, Button, IconButton, Typography, useTheme } from "@mui/material";
 import { tokens } from "../theme";
 import Header from '../Header';
-
+import StatBox from '../StatBox';
+import { GiStethoscope } from "react-icons/gi";
+import EmployeeService from "../../services/EmployeeService";
 
 function AdminDashboard(props) {
   const { isMobile } = props;
   const open = isMobile;
   const theme = useTheme();
   const colors = tokens(theme.palette.mode);
+
+  const [loading, setLoading] = useState(true);
+  const [count, setCount] = useState({
+
+  });
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
+      try {
+        const response = await EmployeeService.getCount();
+        setCount(response.data);
+        console.log(response.data);
+      } catch (error) {
+        console.log(error);
+        console.log("fetch err");
+      }
+      setLoading(false);
+      
+      
+    };
+    fetchData();
+  }, []);
+
 
 
   return (
@@ -25,6 +54,88 @@ function AdminDashboard(props) {
         <Header title="DASHBOARD" subtitle="Welcome to your dashboard" />
 
       </Box>
+
+
+     {/* GRID & CHARTS */}
+     <Box
+        display="grid"
+        gridTemplateColumns="repeat(12, 1fr)"
+        gridAutoRows="140px"
+        gap="20px"
+      >
+        {/* ROW 1 */}
+        <Box
+          gridColumn="span 3"
+          backgroundColor={colors.primary[400]}
+          display="flex"
+          alignItems="center"
+          justifyContent="center"
+        >
+          <StatBox
+            title={count.doctors}
+            subtitle="DOCTORS"
+            
+            icon={
+              <GiStethoscope size={30}
+                sx={{ color: colors.greenAccent[600], fontSize: "26px" }}
+              />
+            }
+          />
+        </Box>
+        <Box
+          gridColumn="span 3"
+          backgroundColor={colors.primary[400]}
+          display="flex"
+          alignItems="center"
+          justifyContent="center"
+        >
+          <StatBox
+            title={count.nurses}
+            subtitle="Nurses"
+            icon={
+              <TbNurse size={30}
+                sx={{ color: colors.greenAccent[600], fontSize: "26px" }}
+              />
+            }
+          />
+        </Box>
+        <Box
+          gridColumn="span 3"
+          backgroundColor={colors.primary[400]}
+          display="flex"
+          alignItems="center"
+          justifyContent="center"
+        >
+          <StatBox
+            title={count.clerks}
+            subtitle="Admission clerks"
+            icon={
+              <CiReceipt size={30}
+                sx={{ color: colors.greenAccent[600], fontSize: "26px" }}
+              />
+            }
+          />
+        </Box>
+        <Box
+          gridColumn="span 3"
+          backgroundColor={colors.primary[400]}
+          display="flex"
+          alignItems="center"
+          justifyContent="center"
+        >
+          <StatBox
+            title={count.patients}
+            subtitle="Active patients"
+            icon={
+              <MdPeopleAlt size={30}
+                sx={{ color: colors.greenAccent[600], fontSize: "26px" }}
+              />
+            }
+          />
+        </Box>
+</Box>
+
+
       </Box>
 
 
--- frontend/src/services/EmployeeService.jsx
+++ frontend/src/services/EmployeeService.jsx
@@ -2,13 +2,16 @@ import axios from "axios";
 
 const EMPLOYEE_API_BASE_URL = "http://localhost:9081/admin/staff";
 const EMPLOYEE_DOCTORS_API_BASE_URL = "http://localhost:9081/admin/doctors";
-
+const ADMIN_API_BASE_URL = "http://localhost:9081/admin";
 class EmployeeService {
   saveEmployee(employee) {
     return axios.post(EMPLOYEE_API_BASE_URL, employee);
   }
   getDoctors(){
     return axios.get(EMPLOYEE_DOCTORS_API_BASE_URL)
+  }
+  getCount(){
+    return axios.get(ADMIN_API_BASE_URL+"/staffCount")
   }
 
 }
