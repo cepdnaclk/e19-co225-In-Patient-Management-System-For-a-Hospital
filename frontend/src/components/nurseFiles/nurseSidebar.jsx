fe498a95443425f67af1690676ea9b8818cb1b59
Isira-Udantha <e19253@eng.pdn.ac.lk>
2023-07-04 11:50:46 +0530

    Correcting Some sidebar errors

--- frontend/src/components/nurseFiles/nurseSidebar.jsx
+++ frontend/src/components/nurseFiles/nurseSidebar.jsx
@@ -1,45 +1,45 @@
 import React from 'react';
-import { useNavigate } from 'react-router-dom';
-import { AiFillDashboard } from 'react-icons/ai';
-import { FaHandHoldingMedical } from 'react-icons/fa';
-import { CgProfile } from 'react-icons/cg';
+import { useNavigate } from "react-router-dom";
+
+import { AiFillDashboard } from "react-icons/ai";
+import { FaHandHoldingMedical } from "react-icons/fa";
+import { CgProfile } from "react-icons/Cg";
 
 function NurseSidebar(props) {
-  const { isMobile } = props;
-  const navigate = useNavigate();
+    const { isMobile } = props;
+    const navigate = useNavigate();
+    const Menus = [
+        { title: "Dashboard", src: <AiFillDashboard size={30} />, path: "/nurse/dashboard" },
+        { title: "Profile", src: <CgProfile size={30} />, path: "/nurse/profile" },
+        //         { title: "Patients", src: <CgProfile size={30} />, path: "/doctor/patients" },
+    ];
 
-  const Menus = [
-    { title: 'Dashboard', icon: <AiFillDashboard size={20} />, path: '/nurse/dashboard' },
-    { title: 'Profile', icon: <CgProfile size={20} />, path: '/nurse/profile' },
-    // { title: 'Patients', icon: <CgProfile size={20} />, path: '/doctor/patients' },
-  ];
+    const navigateDashBoard = (e, path) => {
+        e.preventDefault();
+        navigate(path);
+    }
 
-  const navigateTo = (path) => {
-    navigate(path);
-  };
-
-  const open = isMobile;
-
-  return (
-    <div className={`${open ? 'w-72' : 'w-20'} bg-gray-900 h-screen p-5 pt-8 fixed duration-300`}>
-      <div className="flex items-center mb-6">
-        <FaHandHoldingMedical size={40} color="#e0e7ff" />
-        <h1 className={`text-white ml-2 font-large font-serif italic text-xl duration-200 ${!open && 'scale-0'}`}>MEDNET</h1>
-      </div>
-      <ul>
-        {Menus.map((menu, index) => (
-          <li
-            key={index}
-            onClick={() => navigateTo(menu.path)}
-            className={`flex items-center py-2 cursor-pointer hover:bg-gray-800 transition-colors duration-200 ${menu.gap ? 'mt-9' : 'mt-2'}`}
-          >
-            <span className="mr-3">{menu.icon}</span>
-            <span className={`${!open && 'hidden'} text-white text-sm`}>{menu.title}</span>
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
-}
+    const open = isMobile;
+    return (
+        <>
+            <div className={` ${open ? "w-72" : "w-20 "} bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-gray-700 via-gray-900 to-black h-screen p-5  pt-8 duration-300 fixed`}>
+                <div className="flex gap-x-7 items-center">
+                    <FaHandHoldingMedical size={55} color="#e0e7ff" />
+                    <h1 className={`text-white origin-left font-large font-serif italic text-xl duration-200 ${!open && "scale-0"}`}>MEDNET</h1>
+                </div>
+                <ul className="pt-6">
+                    {Menus.map((Menu, index) => (
+                        <li key={index} onClick={(e, path) => navigateDashBoard(e, Menu.path)} className={`flex  rounded-md p-2 cursor-pointer hover:bg-light-white text-gray-300 text-sm items-center gap-x-4 ${Menu.gap ? "mt-9" : "mt-2"}  `}>
+                            <span>{Menu.src}</span>
+                            <span className={`${!open && "hidden"} origin-left duration-200`}>
+                                <span>{Menu.title}</span>
+                            </span>
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        </>
+    );
+};
 
 export default NurseSidebar;
